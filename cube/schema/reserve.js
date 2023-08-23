d6ed8b3b544191938902c381e81234884a3995a3
Alexey Yurchenko <alexes.dev@gmail.com>
2023-08-23 04:19:04 +0300

    feat(cube/schema/reserve): add usd values

--- cube/schema/reserve.js
+++ cube/schema/reserve.js
@@ -1,20 +1,32 @@
 import { dailySQL } from '../utils';
 
 cube(`reserve`, {
-  sql: dailySQL(['atom_amount', 'fee_amount', 'shortfall_balance', 'total_fee_burned', 'total_fee_minted'], [], `
+  sql: dailySQL(['atom_amount', 'fee_amount', 'shortfall_balance', 'total_fee_burned', 'total_fee_minted', 'atom_price_usd'], [], `
+    with coingecko_history as (
+      select day, array_agg(current_price_usd order by _sdc_received_at desc)[0] as current_price_usd
+        from ${coingecko_history.sql()}
+       where coin_id = 'cosmos'
+       group by 1
+    )
     select block_time -- for dailySQL
          , coalesce(cast(json_value(body, '$.allocations.ATOM.__value') as float64) / pow(10, 6), 0) as atom_amount
          , coalesce(cast(json_value(body, '$.allocations.Fee.__value') as float64) / pow(10, 6), 0) as fee_amount
          , cast(json_value(body, '$.shortfallBalance.__value') as float64) / pow(10, 6) as shortfall_balance
          , cast(json_value(body, '$.totalFeeBurned.__value') as float64) / pow(10, 6) as total_fee_burned
          , cast(json_value(body, '$.totalFeeMinted.__value') as float64) / pow(10, 6) as total_fee_minted
+         , cast(coalesce(ch.current_price_usd) as float64) as atom_price_usd
       from ${state_changes.sql()}
+      left join coingecko_history ch on extract(date from ch.day) = extract(date from block_time)
      where path = 'published.reserve.metrics'
   `),
 
   measures: {
     atom_amount_avg: {
       sql: `atom_amount`,
+      type: `avg`,
+    },
+    atom_amount_usd_avg: {
+      sql: `atom_amount * atom_price_usd`,
       type: `avg`,
     },
     fee_amount_avg: {
@@ -31,6 +43,10 @@ cube(`reserve`, {
     },
     total_fee_burned_avg: {
       sql: `total_fee_burned`,
+      type: `avg`,
+    },
+    total_usd_avg: {
+      sql: `fee_amount + atom_amount * atom_price_usd`,
       type: `avg`,
     },
   },
@@ -52,6 +68,8 @@ cube(`reserve`, {
         shortfall_balance_avg,
         total_fee_minted_avg,
         total_fee_burned_avg,
+        atom_amount_usd_avg,
+        total_usd_avg,
       ],
       time_dimension: day,
       granularity: `day`,
