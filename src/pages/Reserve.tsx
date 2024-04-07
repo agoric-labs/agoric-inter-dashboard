98a6f4399eb4b38a6cbd22f894930dba99d8a005
Fraz Arshad <fraz.arshad12@gmail.com>
2024-04-07 13:14:15 +0500

    feat: added subquery for reserver graph

--- src/pages/Reserve.tsx
+++ src/pages/Reserve.tsx
@@ -6,8 +6,8 @@ import { ReserveSummary } from '@/widgets/ReserveSummary';
 import { ReserveCosmosSummary } from '@/widgets/ReserveCosmosSummary';
 import { ReserveShortfall } from '@/widgets/ReserveShortfall';
 import { ReserveHistory } from '@/widgets/ReserveHistory';
-import { subQueryFetcher } from '@/utils';
-import { RESERVE_DASHBOARD_QUERY } from '@/queries';
+import { subQueryFetcher, subQueryGraphFetcher } from '@/utils';
+import { RESERVE_DASHBOARD_QUERY, RESERVE_GRAPH_TOKENS_QUERY, RESERVE_DAILY_METRICS_QUERY } from '@/queries';
 
 type OraclePriceNode = {
   typeInAmount: number;
@@ -29,6 +29,13 @@ export type ReserveDashboardData = Array<{
   shortfallBalance: number;
   allocations: { [key: string]: AllocationsNode & OraclePriceNode };
 }>;
+
+type ReserveManagerMetricsResponse = {
+  reserveMetrics: { nodes: Array<ReserveMetricsNode> };
+};
+
+type GraphData = { key: number; x: string };
+
 export const Reserve = () => {
   const { data, isLoading } = useSWR<AxiosResponse, AxiosError>(RESERVE_DASHBOARD_QUERY, subQueryFetcher);
 
@@ -48,6 +55,65 @@ export const Reserve = () => {
     ),
   }));
 
+  //  Queries for graph
+  const { data: tokenNamesData } = useSWR<AxiosResponse, AxiosError>(RESERVE_GRAPH_TOKENS_QUERY, subQueryGraphFetcher);
+  const tokenNamesResponse: ReserveManagerMetricsResponse = tokenNamesData?.data.data;
+  const tokenNames =
+    tokenNamesResponse?.reserveMetrics.nodes
+      .map((node) => node.allocations.nodes.map((allocation) => allocation.token))
+      .flat() || [];
+  const { data: dailyMetricsData, isLoading: graphDataIsLoading } = useSWR<AxiosResponse, AxiosError>(
+    RESERVE_DAILY_METRICS_QUERY(tokenNames),
+    subQueryGraphFetcher,
+  );
+  const dailyMetricsResponse = dailyMetricsData?.data.data;
+
+  const graphDataMap: { [key: number]: GraphData } = {};
+  tokenNames.forEach((tokenName) => {
+    const dailyOracles = dailyMetricsResponse?.[`${tokenName}_oracle`].nodes.reduce(
+      (agg: object, dailyOracleData: { dateKey: string }) => ({ ...agg, [dailyOracleData.dateKey]: dailyOracleData }),
+      {},
+    );
+
+    dailyMetricsResponse?.[tokenName].nodes.forEach((dailyTokenMetrics: any) => {
+      const oracle = dailyOracles[dailyTokenMetrics.dateKey] || { typeOutAmountLast: 1, typeInAmountLast: 1 };
+      graphDataMap[dailyTokenMetrics.dateKey] = {
+        ...graphDataMap[dailyTokenMetrics.dateKey],
+        x: dailyTokenMetrics.blockTimeLast.slice(0, 10),
+        key: dailyTokenMetrics.dateKey,
+        [`${dailyTokenMetrics.token}`]:
+          (dailyTokenMetrics.valueLast / 1_000_000) * (oracle.typeOutAmountLast / oracle.typeInAmountLast),
+      };
+    });
+  });
+
+  const sortedGraphDataList = Object.values(graphDataMap).toSorted((a, b) => a.key - b.key);
+  let prevValue: GraphData = sortedGraphDataList[0];
+  const graphDataList: Array<GraphData> = sortedGraphDataList.reduce(
+    (aggArray: Array<GraphData>, graphData: GraphData) => {
+      // filling in missing days
+      const prevDay = new Date(prevValue.x);
+      const nextDay = new Date(graphData.x);
+      const timeDiff = nextDay.getTime() - prevDay.getTime();
+      const diffDays = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+      const missingDays =
+        diffDays > 1
+          ? Array.from(Array(diffDays - 1).keys()).map((idx) => {
+              const newDate = new Date(prevDay);
+              newDate.setDate(prevDay.getDate() + 1 + idx);
+              const newDateString = newDate.toISOString().slice(0, 10);
+              const dateKey = Number(newDateString.replaceAll('-', ''));
+              return { ...prevValue, x: newDateString, key: dateKey };
+            })
+          : [];
+
+      const newAggArray = [...aggArray, ...missingDays, { ...prevValue, ...graphData }];
+      prevValue = { ...prevValue, ...graphData };
+      return newAggArray;
+    },
+    [],
+  );
+
   return (
     <>
       <PageHeader title="Reserve Assets" />
@@ -57,7 +123,7 @@ export const Reserve = () => {
           <ReserveCosmosSummary />
           <ReserveShortfall data={reserveDashboardQueryData} isLoading={isLoading} />
         </div>
-        <ReserveHistory />
+        <ReserveHistory data={graphDataList} tokenNames={tokenNames} isLoading={graphDataIsLoading} />
       </PageContent>
     </>
   );
