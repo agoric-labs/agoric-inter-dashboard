7fd16d3162a518e03bcd46651b4e311ff9435c4e
Fraz Arshad <fraz.arshad12@gmail.com>
2024-04-29 09:55:14 +0500

    chore: new colors added for graph

--- src/components/palette.ts
+++ src/components/palette.ts
@@ -1,1 +1,12 @@
-export const colors = ['#FEC20A', '#D56EEF', '#3292D7', '#D73252'];
+export const colors = [
+    '#FEC20A', // yellow
+    '#D56EEF', // purple 
+    '#3292D7', // blue 
+    '#D73252', // maroon
+    '#3EC1A5', // cyan
+    '#57CD32', // green
+    '#FF0D00', // red
+    '#D87127', // brown
+    '#EB14BC', // pink
+    '#B0B44B', // olive
+];
