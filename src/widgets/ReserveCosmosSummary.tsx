7bdcabf283d9b3ce8adb56a51d9b7070ffdbe1a5
rabi-siddique <rabisiddique27@gmail.com>
2024-05-10 19:24:35 +0500

    refactor: move reserve summary data and network calls in the relevant component

--- src/widgets/ReserveCosmosSummary.tsx
+++ src/widgets/ReserveCosmosSummary.tsx
@@ -1,17 +1,36 @@
-import { Skeleton } from '@/components/ui/skeleton';
 import { ValueCard } from '@/components/ValueCard';
-import { formatPrice } from '@/utils';
+import { GET_ACCOUNT_BALANCE_URL, UIST_DENOMINATION } from '@/constants';
+import { fetchDataFromUrl, formatPrice } from '@/utils';
+import { AxiosResponse, AxiosError } from 'axios';
+import useSWR from 'swr';
+import DataStatus from '@/components/DataStatus';
 
 type Props = {
   title?: string;
-  data: number;
   isLoading: boolean;
+  error: any;
+  reserveAddress: string;
 };
 
-export function ReserveCosmosSummary({ title = 'Cosmos Reserve', data, isLoading }: Props) {
-  if (isLoading || !data) {
-    return <ValueCard title={title} value={<Skeleton className="w-[120px] h-[32px] rounded-full" />} />;
+export function ReserveCosmosSummary({ title = 'Cosmos Reserve', isLoading, reserveAddress, error }: Props) {
+  if (isLoading || error) {
+    return <DataStatus isLoading={isLoading} error={error} title={title} />;
   }
 
-  return <ValueCard title={title} value={formatPrice(data)} />;
+  const {
+    data: reserveBalance,
+    isLoading: reserveBalanceLoading,
+    error: reserveBalanceError,
+  } = useSWR<AxiosResponse, AxiosError>(GET_ACCOUNT_BALANCE_URL(reserveAddress), fetchDataFromUrl);
+
+  if (reserveBalanceLoading || reserveBalanceError) {
+    return <DataStatus isLoading={reserveBalanceLoading} error={reserveBalanceError} title={title} />;
+  }
+
+  const istReserve: { amount: number } = reserveBalance?.data?.balances?.find(
+    (balance: { denom: string }) => balance.denom === UIST_DENOMINATION,
+  );
+  const istReserveBalance = (istReserve?.amount || 0) / 1_000_000;
+
+  return <ValueCard title={title} value={formatPrice(istReserveBalance)} />;
 }
