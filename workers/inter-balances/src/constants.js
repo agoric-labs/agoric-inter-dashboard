e608e2fc9396e4dca4eb74dbf9dd6a2e80f694e4
Fraz Arshad <fraz.arshad12@gmail.com>
2024-05-11 08:50:25 +0500

    fix: updated liquidations data

--- workers/inter-balances/src/constants.js
+++ workers/inter-balances/src/constants.js
@@ -1,3 +1,3 @@
 export const balancesKey = 'balances';
 export const BASE_NODE_URL = 'https://main-a.api.agoric.net:443';
-export const SUBQUERY_URL = 'https://api.subquery.network/sq/agoric-labs/mainnet__YWdvc';
+export const SUBQUERY_URL = 'https://api.subquery.network/sq/agoric-labs/agoric-mainnet-v2__YWdvc';
