eab4deec8123d60924508275016229e4e8f879c6
rabi-siddique <rabisiddique27@gmail.com>
2024-05-13 21:37:04 +0500

    test: add tests for extractDailyOracles

--- tests/Vaults/test.spec.ts
+++ tests/Vaults/test.spec.ts
@@ -1,5 +1,6 @@
 import { constructGraph } from '@/components/VaultCharts';
 import { tokenNames, dailyMetricsResponse } from './mocks';
+import { extractDailyOracles } from '@/utils';
 
 describe('Tests for constructing graph', () => {
   it('should return a list of formatted graph data when given valid input', () => {
@@ -67,5 +68,61 @@ describe('Tests for constructing graph', () => {
     tokenNames = null;
     result = constructGraph(tokenNames as unknown as any, dailyMetricsResponse, graphData);
     expect(result).toEqual(expectedGraphDataList);
+  });
+
+  it('should return an empty object when dailyMetricsResponse is undefined or null', () => {
+    const tokenName = 'ATOM';
+    let dailyMetricsResponse = undefined;
+
+    let result = extractDailyOracles(tokenName, dailyMetricsResponse);
+    expect(result).toEqual({});
+    dailyMetricsResponse = null;
+    result = extractDailyOracles(tokenName, dailyMetricsResponse);
+    expect(result).toEqual({});
+  });
+
+  it('should return an empty object when dailyMetricsResponse has empty nodes array', () => {
+    const tokenName = 'ATOM';
+    const dailyMetricsResponse = {
+      ATOM_oracle: {
+        nodes: [],
+      },
+    };
+
+    const result = extractDailyOracles(tokenName, dailyMetricsResponse);
+
+    expect(result).toEqual({});
+  });
+
+  it('should return a DailyOracles object with the correct dateKey and data when dailyMetricsResponse has valid data', () => {
+    const tokenName = 'ATOM';
+    const result = extractDailyOracles(tokenName, dailyMetricsResponse);
+
+    expect(result).toEqual({
+      '20240511': {
+        blockTimeLast: '2024-05-11T00:08:40',
+        dateKey: 20240511,
+        id: 'ATOM-USD:20240511',
+        typeInAmountLast: 1000000,
+        typeInName: 'ATOM',
+        typeOutAmountLast: 8498855,
+      },
+      '20240512': {
+        blockTimeLast: '2024-05-12T00:04:13',
+        dateKey: 20240512,
+        id: 'ATOM-USD:20240512',
+        typeInAmountLast: 1000000,
+        typeInName: 'ATOM',
+        typeOutAmountLast: 8488890,
+      },
+      '20240513': {
+        blockTimeLast: '2024-05-13T00:09:42',
+        dateKey: 20240513,
+        id: 'ATOM-USD:20240513',
+        typeInAmountLast: 1000000,
+        typeInName: 'ATOM',
+        typeOutAmountLast: 8527726,
+      },
+    });
   });
 });
