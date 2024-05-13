1b8bac3be175804ce8feac71f82bb52bd2dcabb3
rabi-siddique <rabisiddique27@gmail.com>
2024-05-13 16:25:01 +0500

    refactor: create a util function for extracting extractDailyOracles

---
+++ src/types/common.ts
@@ -0,0 +1,18 @@
+export type GraphData = { key: number; x: string; [key: string]: any };
+export type FormattedGraphData = {
+  x: string;
+  key: number;
+  active?: any;
+};
+
+export type DailyMetricsResponse = {
+  [key: string]: {
+    nodes?: { dateKey: string; [key: string]: any }[] | null;
+  };
+};
+
+export type DailyOracles = {
+  [key: string]: { [date: string]: { dateKey: string; [key: string]: any } };
+};
+
+export type TokenDailyOracles = { [date: string]: { dateKey: string; [key: string]: any } };
