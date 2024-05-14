2004ba03af62dabf0cd9a20474bcbb4293e07106
rabi-siddique <rabisiddique27@gmail.com>
2024-05-14 14:30:57 +0500

    test: add tests for reserve graph

---
+++ tests/Reserve/mocks.ts
@@ -0,0 +1,82 @@
+export const tokenNames: string[] = ['ATOM', 'stTIA', 'stkATOM', 'stOSMO', 'stATOM'];
+export const dailyMetricsResponse = {
+  ATOM: {
+    nodes: [
+      {
+        id: 'ATOM:20240513',
+        blockTimeLast: '2024-05-13T08:06:51',
+        dateKey: 20240513,
+        valueLast: '10265620',
+        denom: 'ATOM',
+      },
+      {
+        id: 'ATOM:20240512',
+        blockTimeLast: '2024-05-12T13:06:50',
+        dateKey: 20240512,
+        valueLast: '10265620',
+        denom: 'ATOM',
+      },
+    ],
+  },
+  ATOM_oracle: {
+    nodes: [
+      {
+        dateKey: 20240514,
+        blockTimeLast: '2024-05-14T00:00:27',
+        typeInName: 'ATOM',
+        typeInAmountLast: '1000000',
+        typeOutAmountLast: '8373120',
+      },
+      {
+        dateKey: 20240513,
+        blockTimeLast: '2024-05-13T00:09:42',
+        typeInName: 'ATOM',
+        typeInAmountLast: '1000000',
+        typeOutAmountLast: '8402990',
+      },
+    ],
+  },
+  ATOM_last: {
+    nodes: [
+      {
+        id: 'ATOM:20240213',
+        blockTimeLast: '2024-02-13T14:06:55',
+        dateKey: 20240213,
+        valueLast: '6587020',
+        denom: 'ATOM',
+      },
+    ],
+  },
+  IST: {
+    nodes: [
+      {
+        id: 'IST:20240513',
+        blockTimeLast: '2024-05-13T08:06:51',
+        dateKey: 20240513,
+        valueLast: '68176486663',
+        denom: 'IST',
+      },
+      {
+        id: 'IST:20240512',
+        blockTimeLast: '2024-05-12T13:06:50',
+        dateKey: 20240512,
+        valueLast: '68142083951',
+        denom: 'IST',
+      },
+    ],
+  },
+  IST_oracle: {
+    nodes: [],
+  },
+  IST_last: {
+    nodes: [
+      {
+        id: 'IST:20240213',
+        blockTimeLast: '2024-02-13T14:06:55',
+        dateKey: 20240213,
+        valueLast: '57445941663',
+        denom: 'IST',
+      },
+    ],
+  },
+};
