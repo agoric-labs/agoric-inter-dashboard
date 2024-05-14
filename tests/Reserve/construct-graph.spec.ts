1442c43594737be1cb46f16526138e4a5eb1d7e2
rabi-siddique <rabisiddique27@gmail.com>
2024-05-14 15:00:32 +0500

    chore: ensure generateGraphDataForDateRange returns an empty object is dayRange is null, undefined or empty

--- tests/Reserve/construct-graph.spec.ts
+++ tests/Reserve/construct-graph.spec.ts
@@ -1,6 +1,8 @@
-import { constructGraph } from '@/components/ReserveHistoryGraph';
+import { constructGraph, generateGraphDataForDateRange } from '@/components/ReserveHistoryGraph';
 import { GRAPH_DAYS } from '@/constants';
 import { tokenNames, dailyMetricsResponse } from './mocks';
+import { getDateKey } from '@/utils';
+
 describe('Tests for constructing graph', () => {
   it('should return a list of graph data with length equal to GRAPH_DAYS when given valid tokenNames and dailyMetricsResponse', () => {
     const result = constructGraph(tokenNames, dailyMetricsResponse);
@@ -11,5 +13,27 @@ describe('Tests for constructing graph', () => {
     const dailyMetricsResponse = {};
     const result = constructGraph([], dailyMetricsResponse);
     expect(result).toHaveLength(0);
+  });
+
+  it('should return an empty list when given tokenNames and dailyMetricsResponse are null or undefined', () => {
+    let result = constructGraph(null as any, null);
+    expect(result).toHaveLength(0);
+
+    result = constructGraph(undefined as any, undefined);
+    expect(result).toHaveLength(0);
+  });
+
+  it('should return an empty object if dayRange is an empty array', () => {
+    const result = generateGraphDataForDateRange([]);
+    const expected = {};
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an empty object if dayRange is null or undefined', () => {
+    let result = generateGraphDataForDateRange(null as any);
+    expect(result).toEqual({});
+
+    result = generateGraphDataForDateRange(undefined as any);
+    expect(result).toEqual({});
   });
 });
