7391ecab9576a2f6d8a8c56785ab3f1f23b09108
Fraz Arshad <fraz.arshad12@gmail.com>
2024-05-16 11:59:16 +0500

    fix: fixes for liquidations tests

--- tests/Liquidated.test.tsx
+++ tests/Liquidated.test.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import useSWR from 'swr';
 import { render } from '@testing-library/react';
 import { Liquidated } from '@/pages/Liquidated';
-import { LIQUIDATIONS_DASHBOARD, LIQUIDATION_GRAPH_TOKENS_QUERY } from '@/queries';
-import { graphDataMock, dashboardDataMock, graphNodesDataMock } from './__mocks__/Liquidated';
+import {
+  LIQUIDATIONS_DASHBOARD,
+  LIQUIDATION_GRAPH_TOKENS_QUERY,
+  LIQUIDATION_ORACLE_PRICES_DAILIES_QUERY,
+} from '@/queries';
+import { graphDataMock, dashboardDataMock, graphNodesDataMock, oraclePriceDailies } from './__mocks__/Liquidated';
+import { getDateKey } from '@/utils';
 
 jest.mock('swr', () => ({
   default: jest.fn(),
 }));
-
 
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
@@ -31,10 +35,19 @@ jest.mock('recharts', () => {
 describe('Liquidation Dashboard Snapshot tests', () => {
   it('should match snapshot when data is loaded', async () => {
     (useSWR as any).mockImplementation((query: string) => {
+      const oraclePricesDatekeyMap = dashboardDataMock.data.data.vaultLiquidations.nodes.reduce(
+        (agg: { [key: string]: Array<number> }, vault) => {
+          const { denom } = vault;
+          const prevDenomKeys = agg[denom] || [];
+          return { ...agg, [denom]: [...prevDenomKeys, getDateKey(new Date(vault.blockTime)).key] };
+        },
+        {},
+      );
       const mockData =
         {
           [LIQUIDATIONS_DASHBOARD]: dashboardDataMock,
           [LIQUIDATION_GRAPH_TOKENS_QUERY]: graphNodesDataMock,
+          [LIQUIDATION_ORACLE_PRICES_DAILIES_QUERY(oraclePricesDatekeyMap)]: oraclePriceDailies,
         }[query] || graphDataMock;
       return { data: mockData, error: null, isLoading: false };
     });
