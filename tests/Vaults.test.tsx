97a2835bef74bca315516652cabc0f529a339360
Fraz Arshad <fraz.arshad12@gmail.com>
2024-05-27 19:00:24 +0500

    test: updated vault tests and snapshot

--- tests/Vaults.test.tsx
+++ tests/Vaults.test.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import useSWR from 'swr';
 import { render } from '@testing-library/react';
 import { Vaults } from '@/pages/Vaults';
-import { VAULTS_DASHBOARD_QUERY } from '@/queries';
-import { graphDataMock, dashboardDataMock } from './__mocks__/Vaults';
+import { OPEN_VAULTS_NEXT_PAGES_QUERY, VAULTS_DAILY_METRICS_QUERY, VAULTS_DASHBOARD_QUERY } from '@/queries';
+import { graphDataMock, dashboardDataMock, vaults100Mock, nextPagesMock } from './__mocks__/Vaults';
 
 jest.mock('swr', () => ({
-    default: jest.fn(),
-  }));
-
+  default: jest.fn(),
+}));
 
 global.ResizeObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
@@ -31,10 +30,15 @@ jest.mock('recharts', () => {
 describe('Vaults Dashboard Snapshot tests', () => {
   it('should match snapshot when data is loaded', async () => {
     (useSWR as any).mockImplementation((query: string) => {
-      const mockData =
-        {
-          [VAULTS_DASHBOARD_QUERY]: dashboardDataMock,
-        }[query] || graphDataMock;
+      const tokenNames = dashboardDataMock.data.data.vaultManagerMetrics.nodes.map(
+        (node) => node.liquidatingCollateralBrand,
+      );
+      const pageCount = Math.ceil(dashboardDataMock.data.data.vaults.totalCount / 100) - 1;
+      const mockData = {
+        [VAULTS_DASHBOARD_QUERY]: dashboardDataMock,
+        [OPEN_VAULTS_NEXT_PAGES_QUERY(pageCount)]: null,
+        [VAULTS_DAILY_METRICS_QUERY(tokenNames)]: graphDataMock,
+      }[query];
       return { data: mockData, error: null, isLoading: false };
     });
 
@@ -44,6 +48,52 @@ describe('Vaults Dashboard Snapshot tests', () => {
 
   it('should match snapshot when data is loading', async () => {
     (useSWR as any).mockImplementation(() => ({ data: undefined, error: null, isLoading: true }));
+
+    const { container } = render(<Vaults />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot when openVaults are less than 100', async () => {
+    (useSWR as any).mockImplementation((query: string) => {
+      const modifiedDashboardDataMock: typeof dashboardDataMock = JSON.parse(JSON.stringify(dashboardDataMock));
+      modifiedDashboardDataMock.data.data.vaults.nodes = modifiedDashboardDataMock.data.data.vaults.nodes.slice(0, 99);
+      expect(modifiedDashboardDataMock.data.data.vaults.nodes.length).toBeLessThanOrEqual(100);
+
+      const tokenNames = modifiedDashboardDataMock.data.data.vaultManagerMetrics.nodes.map(
+        (node) => node.liquidatingCollateralBrand,
+      );
+      const pageCount = Math.ceil(modifiedDashboardDataMock.data.data.vaults.totalCount / 100) - 1;
+      expect(pageCount).toBe(0);
+      const mockData = {
+        [VAULTS_DASHBOARD_QUERY]: modifiedDashboardDataMock,
+        [OPEN_VAULTS_NEXT_PAGES_QUERY(pageCount)]: null,
+        [VAULTS_DAILY_METRICS_QUERY(tokenNames)]: graphDataMock,
+      }[query];
+      return { data: mockData, error: null, isLoading: false };
+    });
+
+    const { container } = render(<Vaults />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should match snapshot when openVaults are less greater than 100', async () => {
+    (useSWR as any).mockImplementation((query: string) => {
+      const modifiedDashboardDataMock: typeof dashboardDataMock = JSON.parse(JSON.stringify(dashboardDataMock));
+      modifiedDashboardDataMock.data.data.vaults = vaults100Mock;
+      expect(modifiedDashboardDataMock.data.data.vaults.nodes.length).toEqual(100);
+
+      const tokenNames = modifiedDashboardDataMock.data.data.vaultManagerMetrics.nodes.map(
+        (node) => node.liquidatingCollateralBrand,
+      );
+      const pageCount = Math.ceil(modifiedDashboardDataMock.data.data.vaults.totalCount / 100) - 1;
+      expect(pageCount).toBeGreaterThan(0);
+      const mockData = {
+        [VAULTS_DASHBOARD_QUERY]: modifiedDashboardDataMock,
+        [OPEN_VAULTS_NEXT_PAGES_QUERY(pageCount)]: nextPagesMock,
+        [VAULTS_DAILY_METRICS_QUERY(tokenNames)]: graphDataMock,
+      }[query];
+      return { data: mockData, error: null, isLoading: false };
+    });
 
     const { container } = render(<Vaults />);
     expect(container).toMatchSnapshot();
