7fd53b290dfe94a4fede3d54a6938dab1c182c35
Fraz Arshad <fraz.arshad12@gmail.com>
2024-06-05 13:08:00 +0500

    feat: used VaultStatesDailies for graph

--- src/pages/Liquidated.tsx
+++ src/pages/Liquidated.tsx
@@ -7,16 +7,9 @@ import { LiquidatedVaults } from '@/widgets/LiquidatedVaults';
 import { LiquidatedVaultCountCard } from '@/widgets/LiquidatedVaultCountCard';
 import { VaultStatesChart } from '@/widgets/VaultStatesChart';
 import { populateMissingDays, subQueryFetcher } from '@/utils';
-import { LIQUIDATIONS_DASHBOARD, LIQUIDATION_GRAPH_TOKENS_QUERY, LIQUIDATION_DAILY_METRICS_QUERY } from '@/queries';
-import {
-  GraphData,
-  LiquidationDashboardResponse,
-  LiquidationMetricsDailyResponse,
-  TokenNamesResponse,
-} from '@/types/liquidation-types';
+import { LIQUIDATIONS_DASHBOARD, VAULT_STATE_DAILIES_QUERY } from '@/queries';
+import { GraphData, LiquidationDashboardResponse, VaultStateDailyResponse } from '@/types/liquidation-types';
 import { GRAPH_DAYS, SUBQUERY_STAGING_URL } from '@/constants';
-
-const sum = (items: Array<string>) => items.reduce((agg, next) => agg + Number(next), 0);
 
 export function Liquidated() {
   const { data, isLoading } = useSWR<AxiosResponse, AxiosError>(LIQUIDATIONS_DASHBOARD, (query: string) =>
@@ -34,46 +27,22 @@ export function Liquidated() {
   };
 
   //  Queries for graph
-  const { data: tokenNamesData } = useSWR<AxiosResponse, AxiosError>(LIQUIDATION_GRAPH_TOKENS_QUERY, subQueryFetcher);
-  const tokenNamesResponse: TokenNamesResponse = tokenNamesData?.data.data;
-  const tokenNames = tokenNamesResponse?.vaultManagerMetrics.nodes.map((node) => node.liquidatingCollateralBrand) || [];
-
-  const { data: dailyMetricsData, isLoading: graphDataIsLoading } = useSWR<AxiosResponse, AxiosError>(
-    LIQUIDATION_DAILY_METRICS_QUERY(tokenNames),
-    subQueryFetcher,
+  const { data: vaultStateDailyData, isLoading: graphDataIsLoading } = useSWR<AxiosResponse, AxiosError>(
+    VAULT_STATE_DAILIES_QUERY,
+    (query: string) => subQueryFetcher(query, SUBQUERY_STAGING_URL),
   );
-  const dailyMetricsResponse: LiquidationMetricsDailyResponse = dailyMetricsData?.data?.data;
+  const vaultStateDailyResponse: VaultStateDailyResponse = vaultStateDailyData?.data?.data;
   const graphDataMap: { [key: number]: GraphData } = {};
-  Object.values(dailyMetricsResponse || {}).forEach((tokenDataList) => {
-    tokenDataList?.nodes.forEach((dailyTokenMetrics) => {
-      graphDataMap[dailyTokenMetrics.dateKey] = {
-        ...graphDataMap[dailyTokenMetrics.dateKey],
-        x: dailyTokenMetrics.blockTimeLast.split('T')[0],
-        key: dailyTokenMetrics.dateKey,
-        active: {
-          ...graphDataMap[dailyTokenMetrics.dateKey]?.active,
-          [dailyTokenMetrics.liquidatingCollateralBrand]: dailyTokenMetrics.numActiveVaultsLast,
-        },
-        liquidated: {
-          ...graphDataMap[dailyTokenMetrics.dateKey]?.liquidated,
-          [dailyTokenMetrics.liquidatingCollateralBrand]: dailyTokenMetrics.numLiquidationsCompletedLast,
-        },
-      };
-    });
+  vaultStateDailyResponse?.vaultStatesDailies.nodes?.forEach((vaultState) => {
+    graphDataMap[Number(vaultState.id)] = {
+      x: vaultState.blockTimeLast.split('T')[0],
+      key: Number(vaultState.id),
+      active: Number(vaultState.active),
+      liquidated: Number(vaultState.liquidated),
+    };
   });
 
   const graphDataList = populateMissingDays(graphDataMap, GRAPH_DAYS);
-
-  const summedGraphDataList = graphDataList.map((item) => {
-    const totalActive = item.active ? sum(Object.values(item.active)) : 0;
-    const totalLiquidated = item.liquidated ? sum(Object.values(item.liquidated)) : 0;
-
-    return {
-      ...item,
-      active: totalActive,
-      liquidated: totalLiquidated
-    };
-  });
 
   return (
     <>
@@ -82,7 +51,7 @@ export function Liquidated() {
         <ValueCardGrid>
           <LiquidatedVaultCountCard data={response?.vaultManagerMetrics?.nodes} isLoading={isLoading} />
         </ValueCardGrid>
-        {/* <VaultStatesChart data={summedGraphDataList} isLoading={graphDataIsLoading} /> */}
+        <VaultStatesChart data={graphDataList} isLoading={graphDataIsLoading} />
         <hr className="my-5" />
         <LiquidatedVaults data={liquidationDashboardData} boardAuxes={boardAuxes} isLoading={isLoading} />
       </PageContent>
