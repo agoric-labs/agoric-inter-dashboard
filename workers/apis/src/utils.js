b2cb4bf59d1efe5d90d5a3d9956d47ee0fad452e
Fraz Arshad <fraz.arshad12@gmail.com>
2024-12-16 17:35:01 +0500

    fix: added pagination to gauntlet

--- workers/apis/src/utils.js
+++ workers/apis/src/utils.js
@@ -1,3 +1,5 @@
+import { SUBQUERY_URL } from './constants.js';
+
 export const getDateKey = (date, daysToSubtract = 0) => {
   const dateObject = new Date(date);
   dateObject.setDate(dateObject.getDate() - daysToSubtract);
@@ -5,3 +7,21 @@ export const getDateKey = (date, daysToSubtract = 0) => {
   const startDateKey = Number(startDateFormatDate.replaceAll('-', ''));
   return { key: startDateKey, formattedDate: startDateFormatDate };
 };
+
+export const range = (stop) => [...Object(Array(stop)).keys()];
+
+export const fetchSubquery = async (query) =>
+  await fetch(SUBQUERY_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify(query),
+  });
+
+export const parseFromPaginatedData = (key, paginatedData) =>
+  Object.entries(paginatedData)
+    .filter(([k, _]) => k.split('_')['0'] === key)
+    .map(([_, v]) => v.nodes)
+    .flat();
