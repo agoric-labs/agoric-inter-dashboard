6243bc243cf8f2cf323edb3573a1639f4fba829e
mujahidkay <mujahidkay@gmail.com>
2025-05-30 15:04:56 +0500

    chore: update subquery base url

--- workers/apis/src/constants.js
+++ workers/apis/src/constants.js
@@ -1,3 +1,3 @@
 export const balancesKey = 'balances';
 export const BASE_NODE_URL = 'https://main-a.api.agoric.net:443';
-export const SUBQUERY_URL = 'https://api.subquery.network/sq/agoric-labs/agoric-mainnet-v2';
+export const SUBQUERY_URL = 'https://index-api.onfinality.io/sq/agoric-labs/agoric-mainnet-v2';
